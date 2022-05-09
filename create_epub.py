eae0b81e7f4ebe880b7ec74d1b6592d9b8e50d21
geekylou <nn6215@googlemail.com>
2022-05-09 22:06:24 +0100

    Refactor and add support for defining a book in a json file.

--- create_epub.py
+++ create_epub.py
@@ -3,119 +3,181 @@ from htmldom import htmldom
 from argparse import ArgumentParser
 import urllib.request
 from urllib.parse import urlparse
+from jsonc_parser.parser import JsoncParser
 
-def get_file(url):
-    path = urlparse(url)
-    
-    with urllib.request.urlopen(url) as f:
-        out = f.read() 
-        return out
-        #with open(outfilename, 'wb') as file:
-        #    file.write(out)
+class EpubProcessor:
+    def __init__(self,title="No Title", author="Testy McTestFace"):
+        self.book = epub.EpubBook()
+        self.img_dict = {}
+        # set metadata
+        self.book.set_title(args.title)
+        self.book.set_language('en')
 
-def find_images(book,dom):
-    img_dict = {}
-    imgs = dom.find( "img" )
-    for img in imgs:
-        path = urlparse(img.attr('src'))
-        outfilename = (path.netloc + path.path).replace("/","_")
-        img.attr('src', "imgs/"+outfilename)
+        self.book.add_author(author)
+
+        self.chapters = []
+        self.index=1
+
+    def get_file(self,url):
+        path = urlparse(url)
         
-        img_dict[outfilename] = {}
+        with urllib.request.urlopen(url) as f:
+            out = f.read() 
+            return out
+            #with open(outfilename, 'wb') as file:
+            #    file.write(out)
+
+    def find_images(self,dom):
         
-    for key, value in img_dict.items():
-        content = get_file(key)
+        imgs = dom.find( "img" )
+        for img in imgs:
+            orig_url = img.attr('src')
+            path = urlparse(orig_url)
+            outfilename = (path.netloc + path.path).replace("/","_")
+            img.attr('src', "imgs/"+outfilename)
+            
+            if outfilename not in self.img_dict:
+                self.img_dict[outfilename] = {"orignal_url" : orig_url, "added": False}
+            
+        for key, value in self.img_dict.items():
+            content = self.get_file(value['orignal_url'])
+            
+            if content and value['added'] == False:
+                print(value)
+                img_item = epub.EpubItem(file_name="imgs/"+key, media_type="image/png", content=content)
+                self.book.add_item(img_item)
+                value["added"] = True
+            #with open(key, 'rb') as file:
+            #    print(key)
+            #    
+            #    img_item = epub.EpubItem(file_name="imgs/"+key, media_type="image/png", content=file.read())
+            #    book.add_item(img_item)
+
+    def extract_chapter(self,html_string,container_id):
+
+        dom = htmldom.HtmlDom().createDom(html_string)
+
+        self.find_images(dom)
+
+        title = dom.find( "title" )
         
-        if content:
-            img_item = epub.EpubItem(file_name="imgs/"+key, media_type="image/png", content=content)
-            book.add_item(img_item)
+        content = "<html><head>"
+        content = content + title.html()
+        content = content + "</head>"
         
-        #with open(key, 'rb') as file:
-        #    print(key)
-        #    
-        #    img_item = epub.EpubItem(file_name="imgs/"+key, media_type="image/png", content=file.read())
-        #    book.add_item(img_item)
-def extract_chapter(book,url,container_id):
-    dom = htmldom.HtmlDom(url).createDom()
+        divs = dom.find( "div" )
+        
+        print(container_id)
+        if container_id == None:
+            print("No div id so using raw document.")
+            content= content + dom.find("body").html()
+        else:
+            content = content + "<body>"
+            for div in divs:
+                print(div.attr('id'))
+                if div.attr('id') == container_id:
+                    pass
+                    content = content + div.html()
+        
+            content = content + "</body>"
+        
+        return (title.text(), content)
 
-    find_images(book,dom)
+    def add_chapter(self,html,container_id):
+        title, content = self.extract_chapter(html,container_id)
+        
+        # create chapter  
+        print(title)
+        
+        chapter = epub.EpubHtml(title=title, file_name=('chap_%i.xhtml' % self.index), lang='en')  
+        chapter.content = content
+        self.chapters.append(chapter)
 
-    title = dom.find( "title" )
-    
-    content = "<html><head>"
-    content = content + title.html()
-    content = content + "</head>"
-    
-    divs = dom.find( "div" )
-    
-    print(container_id)
-    if container_id == None:
-        print("No div id so using raw document.")
-        content= content + dom.find("body").html()
-    else:
-        content = content + "<body>"
-        for div in divs:
-            print(div.attr('id'))
-            if div.attr('id') == container_id:
-                pass
-                content = content + div.html()
-    
-        content = content + "</body>"
-    
-    return (title.text(), content)
+        # add chapter
+        self.book.add_item(chapter)
+        self.index=self.index+1
 
-def add_chapter(url,container_id,index,chapters,book):
-    title, content = extract_chapter(book,url,container_id)
-    
-    # create chapter  
-    print(title)
-      
-    chapter = epub.EpubHtml(title=title, file_name=('chap_%i.xhtml' % index), lang='en')  
-    chapter.content = content
-    chapters.append(chapter)
+    def generate_book(self,output_filename):
+        # define Table Of Contents
+        self.book.toc = (self.chapters)
 
-    # add chapter
-    book.add_item(chapter)
+        # add default NCX and Nav file
+        self.book.add_item(epub.EpubNcx())
+        self.book.add_item(epub.EpubNav())
+
+        # define CSS style
+        style = 'BODY {color: white;}'
+        nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+        # add CSS file
+        self.book.add_item(nav_css)
+
+        # basic spine
+        self.book.spine = ['nav'] + self.chapters
+
+        # write to the file
+        epub.write_epub(output_filename, self.book, {})
 
 parser = ArgumentParser()
-parser.add_argument('--div_id',help='foo help')
-parser.add_argument('--title',help='foo help')
-parser.add_argument('--url', action='append',help='foo help')
+parser.add_argument('--div_id',help='id of div to use to get content from in HTML mode')
+parser.add_argument('--title',help='title of book')
+parser.add_argument('--author',help='author')
+parser.add_argument('--json',help='add files as drescribed by the json file')
+parser.add_argument('--html', action='append',help='foo help')
+parser.add_argument('--md', action='append',help="add markdown document to book")
+parser.add_argument('--out',help='output filename')
 args = parser.parse_args()
 
-book = epub.EpubBook()
+epub_processor = EpubProcessor(title=args.title,author=args.author)
 
-# set metadata
-book.set_title(args.title)
-book.set_language('en')
+import os
+from subprocess import Popen, PIPE
 
-book.add_author('Testy McTestFace')
+if args.json:
+    file = JsoncParser.parse_file(args.json)
 
-chapters = []
-index=1
-for url in args.url:
-    print("##### %s looking for '%s'" % (url, args.div_id))
-    div_id = args.div_id
-    
-    add_chapter(url, div_id, index, chapters, book)
-    index=index+1
+    if "cover" in file:
+         # add cover image
+        epub_processor.book.set_cover("image.jpg", open(file["cover"], 'rb').read())
 
-# define Table Of Contents
-book.toc = (chapters)
+    for chapter in file["chapters"]:
+        args_popen = ['pandoc']
 
-# add default NCX and Nav file
-book.add_item(epub.EpubNcx())
-book.add_item(epub.EpubNav())
+        if 'pandoc-format' in chapter:
+            args_popen += ["-f",chapter["pandoc-format"]]
+        
+        args_popen += [chapter["filename"], '--to=HTML']
+        stdout, stderr = Popen(args_popen, stdout=PIPE, stderr=PIPE).communicate()
 
-# define CSS style
-style = 'BODY {color: white;}'
-nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+        content = "<html><title>"+chapter["name"]+"</title><body>"+stdout.decode('utf-8')+"</body></html>"
 
-# add CSS file
-book.add_item(nav_css)
+        epub_processor.add_chapter(content,None)
 
-# basic spine
-book.spine = ['nav'] + chapters
+if args.md:
+    for url in args.md:
+        print("##### %s looking for '%s'" % (url.split(':'), args.div_id))
+        
+        url_split = url.split(':')
 
-# write to the file
-epub.write_epub('test_out.epub', book, {})
+        if len(url_split) > 1:
+            title = url_split[1]
+        else:
+            title = url_split[0]
+
+        filename = url_split[0]
+        args_popen = ['pandoc', filename, '--to=HTML']
+        stdout, stderr = Popen(args_popen, stdout=PIPE, stderr=PIPE).communicate()
+
+        content = "<html><title>"+title+"</title><body>"+stdout.decode('utf-8')+"</body></html>"
+
+        epub_processor.add_chapter(content,None)
+
+if args.html:
+    for url in args.html:
+        print("##### %s looking for '%s'" % (url, args.div_id))
+        div_id = args.div_id
+        
+        with open(url,'r') as file:
+            epub_processor.add_chapter(file.read(), div_id)
+
+epub_processor.generate_book(args.out)
